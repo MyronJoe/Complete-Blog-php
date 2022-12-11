6cb1a0db76340bbb3ce5e62f32b8dcc2b3583974
Myron <josephonyekachi77@gmail.com>
2022-12-11 10:40:18 -0800

    updated

--- admin/posts/index.php
+++ admin/posts/index.php
@@ -1,12 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Our Post</h1>
+<link rel="stylesheet" href="../../assets/css/admin.css">
+<?php
+require_once '../../path.php';
+require_once(ROOT_PATH . '/admin/includes/adminheader.php');
+require_once(ROOT_PATH . '/app/controllers/post.php');
+adminOnly();
+?>
+<section style="display: flex;">
+    <div class="lside">
+        <?php
+        require_once(ROOT_PATH . '/admin/includes/adminsidebar.php');
+        ?>
+    </div>
+
+    <div class="rside">
+        <div class="float-right">
+            <a class="btn btn-primary" href="create.php">Add Post</a>
+        </div>
+        <div class="adduser-sec">
+            <h2>Manage Post</h2>
+        </div>
+
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">S/N</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Section</th>
+                    <th scope="col">Action</th>
+                    <th scope="col">State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($posts as $key => $post) : ?>
+
+                    <tr>
+                        <th scope="row"><?php echo $key + 1 ?></th>
+                        <td><?php echo $post['title'] ?></td>
+                        <td><?php echo $post['category'] ?></td>
+
+                        <td>
+                            <?php if ($post['blog'] && $post['blog'] == 1) : ?>
+                                <p>Blog</p>
+                            <?php else : ?>
+                                <p>Project</p>
+                            <?php endif; ?>
+                        </td>
+
+                        <td>
+                            <a href="edit.php?id=<?php echo $post['id'] ?>" class="btn btn-sm btn-primary mr-1">Edit</a>
+
+                            <a href="index.php?del_id=<?php echo $post['id'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure...?')">Delete</a>
+                        </td>
+
+                        <td>
+                            <?php if ($post['published']) : ?>
+                                <a href="edit.php?published=0&p_id=<?php echo $post['id'] ?>" class="">Unpublish</a>
+                            <?php else : ?>
+                                <a href="edit.php?published=1&p_id=<?php echo $post['id'] ?>" class="">Publish</a>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+    </div>
+</section>
+
 </body>
+
 </html>
