9132238238c77eefbd9d7799af05e367dbfedf7d
Myron <josephonyekachi77@gmail.com>
2022-12-13 22:22:16 -0800

    updated

--- app/database/connect.php
+++ app/database/connect.php
@@ -7,13 +7,4 @@ $db_name = 'frankblog';
 
 $conn = new MySQLi($host, $user, $pass, $db_name);
 
-// checks if the connection was successfull
-
-// if ($conn->connect_error) {
-//     die('Database connection error: '. $conn->connect_error);
-// }
-// else{
-//     echo 'No Error detected';
-// }
-
 ?>
