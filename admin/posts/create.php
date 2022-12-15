be97c8257c8212e1bc66b858afedd05040048444
Myron <josephonyekachi77@gmail.com>
2022-12-14 22:12:22 -0800

    updated

--- admin/posts/create.php
+++ admin/posts/create.php
@@ -6,75 +6,75 @@ require_once(ROOT_PATH . '/app/controllers/posts.php');
 // adminOnly();
 ?>
 <section style="display: flex;">
-    <div class="lside">
-        <?php
-        require_once(ROOT_PATH . '/admin/includes/adminsidebar.php');
-        ?>
+  <div class="lside">
+    <?php
+    require_once(ROOT_PATH . '/admin/includes/adminsidebar.php');
+    ?>
+  </div>
+
+  <div class="rside">
+    <div class="float-right">
+      <a class="btn btn-primary" href="index.php">Manage Post</a>
+    </div>
+    <div class="adduser-sec">
+      <h2>Add Blog Post</h2>
     </div>
 
-    <div class="rside">
-        <div class="float-right">
-            <a class="btn btn-primary" href="index.php">Manage Post</a>
-        </div>
-        <div class="adduser-sec">
-            <h2>Add Blog Post</h2>
-        </div>
+    <form action="create.php" method="POST" class="form" novalidate enctype="multipart/form-data">
 
-        <form action="create.php" method="POST" class="form" novalidate enctype="multipart/form-data">
+      <?php include(ROOT_PATH . "/app/helpers/formerrors.php") ?>
 
-        <?php include(ROOT_PATH . "/app/helpers/formerrors.php") ?>
+      <div class="form-group">
+        <label for="post">Title</label>
+        <input type="text" class="form-control" value="<?php echo $title ?>" id="post" name="title">
+      </div>
 
-        <div class="form-group">
-          <label for="post">Title</label>
-          <input type="text" class="form-control" value="<?php echo $title ?>" id="post" name="title">
-        </div>
+      <div class="form-group">
+        <label for="description">Content</label>
+        <textarea class="form-control" id="description" value="" rows="3" name="body"><?php echo $body ?></textarea>
+      </div>
 
-        <div class="form-group">
-          <label for="description">Content</label>
-          <textarea class="form-control" id="description" value="" rows="3" name="body"><?php echo $body ?></textarea>
-        </div>
+      <div class="form-group">
+        <label for="image">Image</label>
+        <input type="file" class="form-control-file" id="image" name="image">
+      </div>
 
-        <div class="form-group">
-          <label for="image">Image</label>
-          <input type="file" class="form-control-file" id="image" name="image">
-        </div>
+      <div class="form-group">
+        <label for="topic">Topic</label>
+        <select name="topic_id" class="form-control" id="topic">
+          <option value=""></option>
+          <?php foreach ($topics as $key => $topic) : ?>
 
-        <div class="form-group">
-          <label for="topic">Topic</label>
-          <select name="topic_id" class="form-control" id="topic">
-            <option value=""></option>
-            <?php foreach ($topics as $key => $topic) : ?>
+            <?php if (!empty($topic_id) && $topic_id == $topic['id']) : ?>
 
-              <?php if (!empty($topic_id) && $topic_id == $topic['id']) : ?>
-                
-                <option selected value="<?php echo $topic['id']; ?>"><?php echo $topic['name']; ?></option>
-              <?php else : ?>
-                <option value="<?php echo $topic['id']; ?>"><?php echo $topic['name']; ?></option>
-              <?php endif; ?>
+              <option selected value="<?php echo $topic['id']; ?>"><?php echo $topic['name']; ?></option>
+            <?php else : ?>
+              <option value="<?php echo $topic['id']; ?>"><?php echo $topic['name']; ?></option>
+            <?php endif; ?>
 
-            <?php endforeach ?>
+          <?php endforeach ?>
 
-          </select>
-        </div>
+        </select>
+      </div>
 
-        <div class="form-group form-check">
-          <?php if (empty($published)) : ?>
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" name="published">
-            <label class="form-check-label" for="exampleCheck1">Publish</label>
-          <?php else : ?>
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" name="published" checked>
-            <label class="form-check-label" for="exampleCheck1">Publish</label>
-          <?php endif; ?>
-        </div>
+      <div class="form-group form-check">
+        <?php if (empty($published)) : ?>
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" name="published">
+          <label class="form-check-label" for="exampleCheck1">Publish</label>
+        <?php else : ?>
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" name="published" checked>
+          <label class="form-check-label" for="exampleCheck1">Publish</label>
+        <?php endif; ?>
+      </div>
 
-        <button type="submit" name="post-btn" class="btn btn-primary my-2">Add Post</button>
-      </form>
+      <button type="submit" name="post-btn" class="btn btn-primary my-2">Add Post</button>
+    </form>
 
-    </div>
+  </div>
 </section>
 
 <script>
-    CKEDITOR.replace('editor1');
+  CKEDITOR.replace('editor1');
 </script>
 </body>
 
