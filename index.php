6e76ea66316a8b2e6bde3b41cdf2dbb0d571398c
Myron <josephonyekachi77@gmail.com>
2022-12-15 21:21:36 -0800

    updated

--- index.php
+++ index.php
@@ -112,7 +112,10 @@ $entertainment = selectOne('topics', ['name' => 'Entertainment']);
 
 										<a href="<?php echo BASE_URL . '/category.php?t_id=' . $topic['id'] ?>"><?php echo $topic['name'] ?></a>
 									</div>
-									<h3 class="post-title title-lg"><a href="blog-post.html"><?php echo $post['title'] ?></a></h3>
+									<h3 class="post-title title-lg">
+										<a href="post.php?post_id=<?php echo $post['id'] . '&title=' . $post['title'] ?>"><?php echo $post['title'] ?>
+										</a>
+									</h3>
 									<ul class="post-meta">
 
 										<?php if ($post['user_id']) : ?>
