b16b6382f52c7df4dd001f625fda3eeb803c1179
Myron <josephonyekachi77@gmail.com>
2022-12-23 19:56:48 -0800

    updated

--- category.php
+++ category.php
@@ -142,10 +142,10 @@ if (isset($_GET['t_id'])) {
     <!-- /FOOTER -->
 
     <!-- jQuery Plugins -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.stellar.min.js"></script>
+	<script src="assets/js/main.js"></script>
 
 </body>
 
