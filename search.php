f676a0804d3f913586c5aea901dfbee5837dfe58
Myron <josephonyekachi77@gmail.com>
2022-12-23 19:57:51 -0800

    updated

--- search.php
+++ search.php
@@ -147,10 +147,10 @@ if (isset($_POST['term'])) {
     <!-- /FOOTER -->
 
     <!-- jQuery Plugins -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.stellar.min.js"></script>
+	<script src="assets/js/main.js"></script>
 
 </body>
 
