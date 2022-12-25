e91d237f9c2b0301f1e097bd9744dfe577765433
Myron <josephonyekachi77@gmail.com>
2022-12-24 20:00:06 -0800

    updated

--- search.php
+++ search.php
@@ -151,7 +151,5 @@ if (isset($_POST['term'])) {
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/jquery.stellar.min.js"></script>
     <script src="assets/js/main.js"></script>
-
 </body>
-
 </html>
