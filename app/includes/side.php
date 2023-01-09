b5e496cd3feea0df1f4adca41f44cc6b2f116063
Myron <josephonyekachi77@gmail.com>
2023-01-08 22:19:25 -0800

    updated

--- app/includes/side.php
+++ app/includes/side.php
@@ -26,21 +26,6 @@
 </div>
 <!-- /category widget -->
 
-<!-- newsletter widget -->
-<!-- <div class="aside-widget">
-						<div class="section-title">
-							<h2 class="title">Newsletter</h2>
-						</div>
-						<div class="newsletter-widget">
-							<form>
-								<p>Nec feugiat nisl pretium fusce id velit ut tortor pretium.</p>
-								<input class="input" name="newsletter" placeholder="Enter Your Email">
-								<button class="primary-button">Subscribe</button>
-							</form>
-						</div>
-					</div> -->
-<!-- /newsletter widget -->
-
 <!-- post widget -->
 <div class="aside-widget">
     <div class="section-title">
