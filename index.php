fde69533a70efffa5b67adaa7d4a760790e611d1
Myron <josephonyekachi77@gmail.com>
2023-01-11 21:29:39 -0800

    updated

--- index.php
+++ index.php
@@ -276,16 +276,13 @@ $getAllTopics = selectAll('topics');
 							</div>
 						</div>
 					</div>
-		
 
-					<!-- row -->
 					<div class="row">
 						<div class="col-md-12">
 							<div class="section-title">
 								<h2 class="title">Popular Posts</h2>
 							</div>
 						</div>
-						<!-- post -->
 						<?php foreach ($posts as $key => $post) : ?>
                                 <?php if ($key < 3) : ?>
                                     <div class="col-md-4">
@@ -330,8 +327,6 @@ $getAllTopics = selectAll('topics');
                                     </div>
                                 <?php endif; ?>
                             <?php endforeach; ?>
-						<!-- /post -->
-						
 					</div>
 		
 				</div>
@@ -344,7 +339,6 @@ $getAllTopics = selectAll('topics');
 			</div>
 
 		</div>
-		<!-- /container -->
 	</div>
 	
 	<footer id="footer">
